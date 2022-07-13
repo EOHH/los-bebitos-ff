45d46494a914f689831984c065b41f44feaa57b7
GottiHH <107940774+EOHH@users.noreply.github.com>
2022-07-13 09:27:07 -0500

    Add files via upload
    Aaala pe ensalaa ...

---
+++ Js/Seva.js
@@ -0,0 +1,343 @@
+function situacion(x){
+    if (x >= 13){
+      return "Aprobado" 
+    }
+    return "Desaprobado"
+  }
+
+function calc(x, y, z){
+  m = (x + y + z)/3
+  m = Math.round(m)
+  return m
+}
+
+function total(x, y){
+  m = (x*0.4) + (y*0.6)
+  m = Math.round(m)
+  return m
+}
+  
+  function promedio(){
+    var n1 = parseInt(document.getElementById("n1").value)
+    var n1a = parseInt(document.getElementById("n1-1").value)
+    var n1b = parseInt(document.getElementById("n1-2").value)
+    var n1c = parseInt(document.getElementById("n1-3").value)
+    var n1d = parseInt(document.getElementById("n1-4").value)
+    var n1e = parseInt(document.getElementById("n1-5").value)
+
+    var n2 = parseInt(document.getElementById("n2").value)
+    var n2a = parseInt(document.getElementById("n2-1").value)
+    var n2b = parseInt(document.getElementById("n2-2").value)
+    var n2c = parseInt(document.getElementById("n2-3").value)
+    var n2d = parseInt(document.getElementById("n2-4").value)
+    var n2e = parseInt(document.getElementById("n2-5").value)
+
+    var n3 = parseInt(document.getElementById("n3").value)
+    var n3a = parseInt(document.getElementById("n3-1").value)
+    var n3b = parseInt(document.getElementById("n3-2").value)
+    var n3c = parseInt(document.getElementById("n3-3").value)
+    var n3d = parseInt(document.getElementById("n3-4").value)
+    var n3e = parseInt(document.getElementById("n3-5").value)
+
+    var n4 = parseInt(document.getElementById("n4").value)
+    var n4a = parseInt(document.getElementById("n4-1").value)
+    var n4b = parseInt(document.getElementById("n4-2").value)
+    var n4c = parseInt(document.getElementById("n4-3").value)
+    var n4d = parseInt(document.getElementById("n4-4").value)
+    var n4e = parseInt(document.getElementById("n4-5").value)
+    
+    var n5 = parseInt(document.getElementById("n5").value)
+    var n5a = parseInt(document.getElementById("n5-1").value)
+    var n5b = parseInt(document.getElementById("n5-2").value)
+    var n5c = parseInt(document.getElementById("n5-3").value)
+    var n5d = parseInt(document.getElementById("n5-4").value)
+    var n5e = parseInt(document.getElementById("n5-5").value)
+
+    var n6 = parseInt(document.getElementById("n6").value)
+    var n6a = parseInt(document.getElementById("n6-1").value)
+    var n6b = parseInt(document.getElementById("n6-2").value)
+    var n6c = parseInt(document.getElementById("n6-3").value)
+    var n6d = parseInt(document.getElementById("n6-4").value)
+    var n6e = parseInt(document.getElementById("n6-5").value)
+
+    var n7 = parseInt(document.getElementById("n7").value)
+    var n7a = parseInt(document.getElementById("n7-1").value)
+    var n7b = parseInt(document.getElementById("n7-2").value)
+    var n7c = parseInt(document.getElementById("n7-3").value)
+    var n7d = parseInt(document.getElementById("n7-4").value)
+    var n7e = parseInt(document.getElementById("n7-5").value)
+
+    var n8 = parseInt(document.getElementById("n8").value)
+    var n8a = parseInt(document.getElementById("n8-1").value)
+    var n8b = parseInt(document.getElementById("n8-2").value)
+    var n8c = parseInt(document.getElementById("n8-3").value)
+    var n8d = parseInt(document.getElementById("n8-4").value)
+    var n8e = parseInt(document.getElementById("n8-5").value)
+
+    var n9 = parseInt(document.getElementById("n9").value)
+    var n9a = parseInt(document.getElementById("n9-1").value)
+    var n9b = parseInt(document.getElementById("n9-2").value)
+    var n9c = parseInt(document.getElementById("n9-3").value)
+    var n9d = parseInt(document.getElementById("n9-4").value)
+    var n9e = parseInt(document.getElementById("n9-5").value)
+
+    var n10 = parseInt(document.getElementById("n10").value)
+    var n10a = parseInt(document.getElementById("n10-1").value)
+    var n10b = parseInt(document.getElementById("n10-2").value)
+    var n10c = parseInt(document.getElementById("n10-3").value)
+    var n10d = parseInt(document.getElementById("n10-4").value)
+    var n10e = parseInt(document.getElementById("n10-5").value)
+
+    var n11 = parseInt(document.getElementById("n11").value)
+    var n11a = parseInt(document.getElementById("n11-1").value)
+    var n11b = parseInt(document.getElementById("n11-2").value)
+    var n11c = parseInt(document.getElementById("n11-3").value)
+    var n11d = parseInt(document.getElementById("n11-4").value)
+    var n11e = parseInt(document.getElementById("n11-5").value)
+
+    var n12 = parseInt(document.getElementById("n12").value)
+    var n12a = parseInt(document.getElementById("n12-1").value)
+    var n12b = parseInt(document.getElementById("n12-2").value)
+    var n12c = parseInt(document.getElementById("n12-3").value)
+    var n12d = parseInt(document.getElementById("n12-4").value)
+    var n12e = parseInt(document.getElementById("n12-5").value)
+
+    var n13 = parseInt(document.getElementById("n13").value)
+    var n13a = parseInt(document.getElementById("n13-1").value)
+    var n13b = parseInt(document.getElementById("n13-2").value)
+    var n13c = parseInt(document.getElementById("n13-3").value)
+    var n13d = parseInt(document.getElementById("n13-4").value)
+    var n13e = parseInt(document.getElementById("n13-5").value)
+
+    var n14 = parseInt(document.getElementById("n14").value)
+    var n14a = parseInt(document.getElementById("n14-1").value)
+    var n14b = parseInt(document.getElementById("n14-2").value)
+    var n14c = parseInt(document.getElementById("n14-3").value)
+    var n14d = parseInt(document.getElementById("n14-4").value)
+    var n14e = parseInt(document.getElementById("n14-5").value)
+
+    var n15 = parseInt(document.getElementById("n15").value)
+    var n15a = parseInt(document.getElementById("n15-1").value)
+    var n15b = parseInt(document.getElementById("n15-2").value)
+    var n15c = parseInt(document.getElementById("n15-3").value)
+    var n15d = parseInt(document.getElementById("n15-4").value)
+    var n15e = parseInt(document.getElementById("n15-5").value)
+
+    var n16 = parseInt(document.getElementById("n16").value)
+    var n16a = parseInt(document.getElementById("n16-1").value)
+    var n16b = parseInt(document.getElementById("n16-2").value)
+    var n16c = parseInt(document.getElementById("n16-3").value)
+    var n16d = parseInt(document.getElementById("n16-4").value)
+    var n16e = parseInt(document.getElementById("n16-5").value)
+
+    var n17 = parseInt(document.getElementById("n17").value)
+    var n17a = parseInt(document.getElementById("n17-1").value)
+    var n17b = parseInt(document.getElementById("n17-2").value)
+    var n17c = parseInt(document.getElementById("n17-3").value)
+    var n17d = parseInt(document.getElementById("n17-4").value)
+    var n17e = parseInt(document.getElementById("n17-5").value)
+
+    var n18 = parseInt(document.getElementById("n18").value)
+    var n18a = parseInt(document.getElementById("n18-1").value)
+    var n18b = parseInt(document.getElementById("n18-2").value)
+    var n18c = parseInt(document.getElementById("n18-3").value)
+    var n18d = parseInt(document.getElementById("n18-4").value)
+    var n18e = parseInt(document.getElementById("n18-5").value)
+
+    var n19 = parseInt(document.getElementById("n19").value)
+    var n19a = parseInt(document.getElementById("n19-1").value)
+    var n19b = parseInt(document.getElementById("n19-2").value)
+    var n19c = parseInt(document.getElementById("n19-3").value)
+    var n19d = parseInt(document.getElementById("n19-4").value)
+    var n19e = parseInt(document.getElementById("n19-5").value)
+
+    var aula1 = calc(n1, n1a, n1b)
+    var lab1 = calc(n1c, n1d, n1e)
+    var total1 = total(aula1, lab1)
+    var situacion1 = situacion(total1)
+    
+    var aula2 = calc(n2, n2a, n2b) 
+    var lab2 = calc(n2c, n2d, n2e) 
+    var total2 = total(aula2, lab2)
+    var situacion2 = situacion(total2)
+
+    var aula3 = calc(n3, n3a, n3b) 
+    var lab3 = calc(n3c, n3d, n3e) 
+    var total3 = total(aula3, lab3)
+    var situacion3 = situacion(total3)
+
+    var aula4 = calc(n4, n4a, n4b) 
+    var lab4 = calc(n4c, n4d, n4e) 
+    var total4 = total(aula4, lab4)
+    var situacion4 = situacion(total4)
+
+    var aula5 = calc(n5, n5a, n5b) 
+    var lab5 = calc(n5c, n5d, n5e) 
+    var total5 = total(aula5, lab5)
+    var situacion5 = situacion(total5)
+
+    var aula6 = calc(n6, n6a, n6b) 
+    var lab6 = calc(n6c, n6d, n6e) 
+    var total6 = total(aula6, lab6)
+    var situacion6 = situacion(total6) 
+
+    var aula7 = calc(n7, n7a, n7b) 
+    var lab7 = calc(n7c, n7d, n7e) 
+    var total7 = total(aula7, lab7)    
+    var situacion7 = situacion(total7)
+
+    var aula8 = calc(n8, n8a, n8b) 
+    var lab8 = calc(n8c + n8d + n8e) 
+    var total8 = total(aula8, lab8)
+    var situacion8 = situacion(total8) 
+
+    var aula9 = calc(n9, n9a, n9b) 
+    var lab9 = calc(n9c + n9d + n9e) 
+    var total9 = total(aula9, lab9)
+    var situacion9 = situacion(total9) 
+
+    var aula10 = calc(n10, n10a, n10b)
+    var lab10 = calc(n10c, n10d, n10e) 
+    var total10 = total(aula10, lab10)
+    var situacion10 = situacion(total10)
+
+    var aula11 = calc(n11, n11a, n11b) 
+    var lab11 = calc(n11c, n11d, n11e) 
+    var total11 = total(aula11, lab11)
+    var situacion11 = situacion(total11) 
+
+    var aula12 = calc(n12, n12a, n12b) 
+    var lab12 = calc(n12c, n12d, n12e) 
+    var total12 = total(aula12, lab12)
+    var situacion12 = situacion(total12) 
+
+    var aula13 = calc(n13, n13a, n13b) 
+    var lab13 = calc(n13c, n13d, n13e) 
+    var total13 = total(aula13, lab13)
+    var situacion13 = situacion(total13) 
+
+    var aula14 = calc(n14, n14a, n14b) 
+    var lab14 = calc(n14c, n14d, n14e) 
+    var total14 = total(aula14, lab14)
+    var situacion14 = situacion(total14) 
+
+    var aula15 = calc(n15, n15a, n15b) 
+    var lab15 = calc(n15c, n15d, n15e) 
+    var total15 = total(aula15, lab15)
+    var situacion15 = situacion(total15) 
+
+    var aula16 = calc(n16, n16a, n16b) 
+    var lab16 = calc(n16c, n16d, n16e) 
+    var total16 = total(aula16, lab16)
+    var situacion16 = situacion(total16) 
+
+    var aula17 = calc(n17, n17a, n17b) 
+    var lab17 = calc(n17c, n17d, n17e) 
+    var total17 = total(aula17, lab17)
+    var situacion17 = situacion(total17) 
+    
+    var aula18 = calc(n18, n18a, n18b) 
+    var lab18 = calc(n18c, n18d, n18e) 
+    var total18 = total(aula18, lab18)
+    var situacion18 = situacion(total18) 
+
+    var aula19 = calc(n19, n19a, n19b) 
+    var lab19 = calc(n19c, n19d, n19e) 
+    var total19 = total(aula19, lab19)
+    var situacion19 = situacion(total19) 
+    
+    document.getElementById("p1").value = aula1
+    document.getElementById("p1-1").value = lab1
+    document.getElementById("p1-2").value = total1
+    document.getElementById("situacion1").value = situacion1
+
+    document.getElementById("p2").value = aula2
+    document.getElementById("p2-1").value = lab2
+    document.getElementById("p2-2").value = total2
+    document.getElementById("situacion2").value = situacion2
+
+    document.getElementById("p3").value = aula3
+    document.getElementById("p3-1").value = lab3
+    document.getElementById("p3-2").value = total3
+    document.getElementById("situacion3").value = situacion3
+
+    document.getElementById("p4").value = aula4
+    document.getElementById("p4-1").value = lab4
+    document.getElementById("p4-2").value = total4
+    document.getElementById("situacion4").value = situacion4
+
+    document.getElementById("p5").value = aula5
+    document.getElementById("p5-1").value = lab5
+    document.getElementById("p5-2").value = total5
+    document.getElementById("situacion5").value = situacion5
+
+    document.getElementById("p6").value = aula6
+    document.getElementById("p6-1").value = lab6
+    document.getElementById("p6-2").value = total6
+    document.getElementById("situacion6").value = situacion6
+
+    document.getElementById("p7").value = aula7
+    document.getElementById("p7-1").value = lab7
+    document.getElementById("p7-2").value = total7
+    document.getElementById("situacion7").value = situacion7
+
+    document.getElementById("p8").value = aula8
+    document.getElementById("p8-1").value = lab8
+    document.getElementById("p8-2").value = total8
+    document.getElementById("situacion8").value = situacion8
+
+    document.getElementById("p9").value = aula9
+    document.getElementById("p9-1").value = lab9
+    document.getElementById("p9-2").value = total9
+    document.getElementById("situacion9").value = situacion9
+
+    document.getElementById("p10").value = aula10
+    document.getElementById("p10-1").value = lab10
+    document.getElementById("p10-2").value = total10
+    document.getElementById("situacion10").value = situacion10
+
+    document.getElementById("p11").value = aula11
+    document.getElementById("p11-1").value = lab11
+    document.getElementById("p11-2").value = total11
+    document.getElementById("situacion11").value = situacion11
+
+    document.getElementById("p12").value = aula12
+    document.getElementById("p12-1").value = lab12
+    document.getElementById("p12-2").value = total12
+    document.getElementById("situacion12").value = situacion12
+
+    document.getElementById("p13").value = aula13
+    document.getElementById("p13-1").value = lab13
+    document.getElementById("p13-2").value = total13
+    document.getElementById("situacion13").value = situacion13
+
+    document.getElementById("p14").value = aula14
+    document.getElementById("p14-1").value = lab14
+    document.getElementById("p14-2").value = total14
+    document.getElementById("situacion14").value = situacion14
+
+    document.getElementById("p15").value = aula15
+    document.getElementById("p15-1").value = lab15
+    document.getElementById("p15-2").value = total15
+    document.getElementById("situacion15").value = situacion15
+
+    document.getElementById("p16").value = aula16
+    document.getElementById("p16-1").value = lab16
+    document.getElementById("p16-2").value = total16
+    document.getElementById("situacion16").value = situacion16
+
+    document.getElementById("p17").value = aula17
+    document.getElementById("p17-1").value = lab17
+    document.getElementById("p17-2").value = total17
+    document.getElementById("situacion17").value = situacion17
+
+    document.getElementById("p18").value = aula18
+    document.getElementById("p18-1").value = lab18
+    document.getElementById("p18-2").value = total18
+    document.getElementById("situacion18").value = situacion18
+
+    document.getElementById("p19").value = aula19
+    document.getElementById("p19-1").value = lab19
+    document.getElementById("p19-2").value = total19
+    document.getElementById("situacion19").value = situacion19
+  }
